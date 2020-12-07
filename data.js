88de701509c97e0291c7a16056f2db8ced588090
Anna Makagonova <makagonova.ann@gmail.com>
2020-12-06 21:58:38 -0500

    change files

---
+++ data.js
@@ -0,0 +1,12 @@
+// var req = new XMLHttpRequest();
+// req.open('GET', 'https://adventofcode.com/2020/day/1/input', false);
+// req.send(null);
+// if(req.status == 200)
+//    dump(req.responseText);
+
+// const axios = require("axios");
+
+// axios
+//   .get("https://adventofcode.com/2020/day/1/input")
+//   .then(({ data }) => console.log("data", data))
+//   .catch((err) => console.log("err", err));
