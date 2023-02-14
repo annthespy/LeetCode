2e49ccc1881db63d8547d935373e40f8256d77a5
Anna Makagonova <makagonova.ann@gmail.com>
2023-02-14 17:03:43 -0500

    Time: 77 ms (56.83%), Space: 46.5 MB (7.65%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} key
+ * @param {string} message
+ * @return {string}
+ */
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var decodeMessage = function(key, message) {
+  let cypher = {}
+  let pointer = 0
+    for (var i = 0; i < key.length; i++) {
+      if (key[i] === ' ') {
+      }
+      if (!cypher[key[i]] && key[i] !== ' ') {
+        cypher[key[i]] = alphabet[pointer]
+        pointer++
+      }
+    }
+  console.log(cypher)
+  let result = ''
+  for (var i = 0; i < message.length; i++) {
+    if (message[i] === ' ') {
+      result = result + ' '
+    } else {
+      result = result + cypher[message[i]]
+    }
+  }
+  return result
+};
