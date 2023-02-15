8b6faf9fd4b50d5a987504773aa412923884479c
Anna Makagonova <makagonova.ann@gmail.com>
2023-02-15 13:03:06 -0500

    Time: 85 ms (23.97%), Space: 43.1 MB (33.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  let result = ''
+    for (var i = 0; i < strs[0].length; i++) {
+      for (var j = 1; j < strs.length; j++) {
+        if (strs[0][i] !== strs[j][i]) {
+          return strs[0].slice(0, i);
+        }
+      }
+    }   
+ return strs[0]
+};
