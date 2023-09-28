0413df478305267630bf15fe3246590949108826
Anna Makagonova <makagonova.ann@gmail.com>
2023-09-28 12:48:44 -0400

    Time: 68 ms (74.80%), Space: 51.7 MB (42.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,19 +2,19 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = (prices) => {
-  let left = 0; // Buy
-  let right = 1; // sell
-  let max_profit = 0;
-  while (right < prices.length) {
-    if (prices[left] < prices[right]) {
-      let profit = prices[right] - prices[left]; // our current profit
-
-      max_profit = Math.max(max_profit, profit);
-    } else {
-      left = right;
+var maxProfit = function(prices) {
+    let profit = 0
+    let minPrice = +Infinity
+    
+    // [7,1,5,3,6,4]
+    for (var i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) { 
+            minPrice = prices[i]
+        }
+        else if (prices[i] - minPrice > profit) {
+            profit = prices[i] - minPrice
+        }
     }
-    right++;
-  }
-  return max_profit;
+    
+    return profit
 };
