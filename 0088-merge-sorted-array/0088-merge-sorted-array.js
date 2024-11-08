5df40fea936420495398a7ad9e68ad715aa98291
Anna Makagonov <makagonova.ann@gmail.com>
2024-11-07 19:28:45 -0500

    Time: 0 ms (100%), Space: 49.1 MB (41.58%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,7 +6,14 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    nums1.splice(m)
-    nums1.push(...nums2)
-    nums1.sort(function(a,b){return a - b})
+    // nums1.splice(m, nums1.length - m)
+    // nums1 = nums1.concat(nums2)
+    // nums1.sort((a, b) => a - b)
+    // console.log('nums1', nums1)
+    for (let i=0; i<m+n; i++) {
+        if (i>=m) {
+            nums1[i] = nums2[i-m]
+        }
+    }
+    nums1.sort((a, b) => (a-b));
 };
